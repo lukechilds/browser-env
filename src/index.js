253edbd1c0ebbb048b072a999ea3146ef78f031f
Luke Childs <lukechilds123@gmail.com>
2016-05-24 22:37:45 +0100

    Remove arrow function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 global.window = require('jsdom').jsdom('<html><body></body></html>').defaultView;
-Object.keys(window).forEach(prop => !global[prop] && (global[prop] = window[prop]));
+Object.keys(window).forEach(function(prop) {
+  if(!global[prop]) {
+    global[prop] = window[prop];
+  }
+});
 
 module.exports = window;
