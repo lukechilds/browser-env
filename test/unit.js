270121c9b53f82cc7a781e4f77728bf88949400a
Luke Childs <lukechilds123@gmail.com>
2016-05-24 23:11:18 +0100

    Add tests

---
+++ test/unit.js
@@ -0,0 +1,14 @@
+import test from 'ava';
+import browserEnv from '../src';
+
+test('Function should setup browser environment', t => {
+  t.is(typeof window, 'undefined');
+  t.is(typeof document, 'undefined');
+  t.is(typeof navigator, 'undefined');
+  t.is(typeof HTMLElement, 'undefined');
+  browserEnv();
+  t.not(typeof window, 'undefined');
+  t.not(typeof document, 'undefined');
+  t.not(typeof navigator, 'undefined');
+  t.not(typeof HTMLElement, 'undefined');
+});
