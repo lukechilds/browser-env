d7593ec998551a6ab71b08543ecd2d00895cf02e
Luke Childs <lukechilds123@gmail.com>
2016-05-24 22:59:11 +0100

    Wrap module in function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-global.window = require('jsdom').jsdom('<html><body></body></html>').defaultView;
-Object.keys(window).forEach(function(prop) {
-  if(typeof global[prop] === 'undefined') {
-    global[prop] = window[prop];
-  }
-});
+var jsdom = require('jsdom').jsdom;
 
-module.exports = window;
+module.exports = function() {
+  global.window = jsdom('<html><body></body></html>').defaultView;
+  
+  Object.keys(window).forEach(function(prop) {
+    if(typeof global[prop] === 'undefined') {
+      global[prop] = window[prop];
+    }
+  });
+
+  return window;
+};
