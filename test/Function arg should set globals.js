3af72530b3eda2112fd6bdd63ca780215b5630bd
Luke Childs <lukechilds123@gmail.com>
2016-05-25 23:34:59 +0100

    Add test for global properties arg

---
+++ test/Function arg should set globals.js
@@ -0,0 +1,19 @@
+import test from 'ava';
+import browserEnv from '../dist';
+
+test(t => {
+  t.is(typeof window, 'undefined');
+  t.is(typeof document, 'undefined');
+  t.is(typeof navigator, 'undefined');
+  t.is(typeof HTMLElement, 'undefined');
+  browserEnv([]);
+  t.is(typeof window, 'undefined');
+  t.is(typeof document, 'undefined');
+  t.is(typeof navigator, 'undefined');
+  t.is(typeof HTMLElement, 'undefined');
+  browserEnv(['navigator', 'HTMLElement']);
+  t.is(typeof window, 'undefined');
+  t.is(typeof document, 'undefined');
+  t.not(typeof navigator, 'undefined');
+  t.not(typeof HTMLElement, 'undefined');
+});
