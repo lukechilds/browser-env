5277f37c5d83ff7173e3163169e4a6b206fcc873
Luke Childs <lukechilds123@gmail.com>
2016-05-25 23:17:52 +0100

    Don't make window global until loop

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { jsdom } from 'jsdom';
 
 module.exports = () => {
-  global.window = jsdom('<html><body></body></html>').defaultView;
+  const window = jsdom('<html><body></body></html>').defaultView;
 
   Object.keys(window).forEach(prop => {
     if(typeof global[prop] === 'undefined') {
