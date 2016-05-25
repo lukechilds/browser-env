68ab0bafe6c52522b52876d829ef21ad1cc3f92d
Luke Childs <lukechilds123@gmail.com>
2016-05-25 23:29:45 +0100

    Add param to choose globals

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { jsdom } from 'jsdom';
 
-module.exports = () => {
+module.exports = (props) => {
   const window = jsdom('<html><body></body></html>').defaultView;
 
   Object.keys(window).forEach(prop => {
     if(typeof global[prop] === 'undefined') {
+      if(props instanceof Array && props.indexOf(prop) === -1) {
+        return;
+      }
       global[prop] = window[prop];
     }
   });
