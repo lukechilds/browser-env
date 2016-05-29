f26061d83647cc2cf837825a852c2c259d5fbaab
Luke Childs <lukechilds123@gmail.com>
2016-05-29 12:28:06 +0100

    Add overwrite arg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { jsdom } from 'jsdom';
 
-module.exports = (props) => {
+module.exports = (props, overwrite) => {
+  const presetProps = props instanceof Array ? props : false;
   const window = jsdom('<html><body></body></html>').defaultView;
 
   Object
     .keys(window)
-    .filter(prop => typeof global[prop] === 'undefined')
-    .filter(prop => !(props instanceof Array && props.indexOf(prop) === -1))
+    .filter(prop => typeof global[prop] === 'undefined' || presetProps && overwrite)
+    .filter(prop => !(presetProps && props.indexOf(prop) === -1))
     .forEach(prop => global[prop] = window[prop]);
 
   return window;
