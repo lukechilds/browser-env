9ca08b2bdd043cab3592686c6fa2de9a54b4e0f5
Luke Childs <lukechilds123@gmail.com>
2016-06-19 16:51:44 +0100

    Always overwrite globals but don't overwrite node globals

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import { jsdom } from 'jsdom';
 
+const protectedproperties = (() => {
+  const window = jsdom('<html><body></body></html>').defaultView;
+
+  return Object
+    .keys(window)
+    .filter(prop => global[prop]);
+})();
+
 module.exports = arg => {
   const properties = arg instanceof Array ? arg : arg && arg.properties;
-  const overwrite = arg && arg.overwrite;
   const window = jsdom('<html><body></body></html>').defaultView;
 
   Object
     .keys(window)
-    .filter(prop => typeof global[prop] === 'undefined' || properties && overwrite)
+    .filter(prop => protectedproperties.indexOf(prop) === -1)
     .filter(prop => !(properties && properties.indexOf(prop) === -1))
     .forEach(prop => global[prop] = window[prop]);
 
