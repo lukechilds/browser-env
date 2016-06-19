b3b707278b84e9f56733787b73576eeca200a1d9
Luke Childs <lukechilds123@gmail.com>
2016-06-19 15:56:31 +0100

    Move advanced settings into config object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { jsdom } from 'jsdom';
 
-module.exports = (props, overwrite) => {
-  const presetProps = props instanceof Array ? props : false;
+module.exports = (arg) => {
+  const properties = arg instanceof Array ? arg : arg && arg.properties;
+  const overwrite = arg && arg.overwrite;
   const window = jsdom('<html><body></body></html>').defaultView;
 
   Object
     .keys(window)
-    .filter(prop => typeof global[prop] === 'undefined' || presetProps && overwrite)
-    .filter(prop => !(presetProps && props.indexOf(prop) === -1))
+    .filter(prop => typeof global[prop] === 'undefined' || properties && overwrite)
+    .filter(prop => !(properties && properties.indexOf(prop) === -1))
     .forEach(prop => global[prop] = window[prop]);
 
   return window;
