ccb35de8b80a43357f8482af137f8eb117906498
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2019-02-04 11:35:52 +0100

    Since AVA@0.25, all tests should have a title.

--- test/window-properties-should-be-as-expected.js
+++ test/window-properties-should-be-as-expected.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import browserEnv from '../src';
 import expectedProperties from './fixtures/expectedProperties';
 
-test(t => {
+test('Window properties should be as expected', t => {
 	browserEnv();
 	const properties = Object.getOwnPropertyNames(window);
 	t.deepEqual(expectedProperties.sort(), properties.sort());
