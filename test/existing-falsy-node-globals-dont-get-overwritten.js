a926ee9cddd80b6206e013a8dc68b3c3c5ec915a
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2019-03-26 17:57:02 +0100

    Revert moving import so we can set global before.

--- test/existing-falsy-node-globals-dont-get-overwritten.js
+++ test/existing-falsy-node-globals-dont-get-overwritten.js
@@ -1,10 +1,9 @@
 import test from 'ava';
-import browserEnv from '../src';
 
 // We have to require() here as imports have to be top level so we can't set
 // globals first
 test('Existing falsy node globals don\'t get overwritten', t => {
 	global.document = false;
-	browserEnv();
+	require('../src')();
 	t.is(document, false);
 });
